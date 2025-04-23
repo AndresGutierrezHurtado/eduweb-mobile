e82a1d70cd8643d4da4a84b44b6dc34bc8291433
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-04-22 23:20:28 -0500

    fix: update text styles and layout adjustments in index component

--- app/(auth)/index.jsx
+++ app/(auth)/index.jsx
@@ -14,20 +14,17 @@ export default function Index() {
                     style={{ height: 200, width: 200 }}
                     className="mb-4"
                 />
-                <Text
-                    className="text-5xl text-base-content"
-                    style={{ fontFamily: "alegreya-sans" }}
-                >
+                <Text className="text-5xl text-base-content font-Alegreya">
                     Edu<Text className="text-primary">Web</Text>
                 </Text>
             </View>
-            <View className="flex-1 items-center justify-center bg-sky-50 rounded-t-xl p-10">
+            <View className="h-2/5 items-center justify-center bg-sky-50 rounded-t-xl p-10">
                 <View className="flex-1 items-center justify-center gap-12 w-full">
                     <View className="flex-col gap-2">
-                        <Text className="text-base-100 text-3xl font-extrabold text-center">
+                        <Text className="text-base-100 text-4xl font-extrabold text-center leading-none">
                             Bienvenido a EduWeb
                         </Text>
-                        <Text className="text-base-300/80 text-lg font-medium text-center">
+                        <Text className="text-base-300 text-lg text-center leading-none">
                             Inicia sesión o crea una cuenta para continuar
                         </Text>
                     </View>
@@ -35,16 +32,16 @@ export default function Index() {
                         <Link
                             href="/login"
                             asChild
-                            className="border-2 border-base-300  rounded-full px-5 py-2 w-full"
+                            className="border-2 border-base-300 bg-base-300 rounded-lg px-5 py-2 w-full"
                         >
-                            <Text className="text-base-300 text-lg font-bold text-center">
+                            <Text className="text-base-content text-lg font-bold text-center">
                                 Iniciar Sesión
                             </Text>
                         </Link>
                         <Link
                             href="/register"
                             asChild
-                            className="border-2 border-base-300  rounded-full px-5 py-2 w-full"
+                            className="border-2 border-base-300 rounded-lg px-5 py-2 w-full"
                         >
                             <Text className="text-base-300 text-lg font-bold text-center">
                                 Crear Cuenta
